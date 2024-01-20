ce7e5614287548fcef2ec9fe6e3344af620f40a4
박재영 <140151810+ttuttae@users.noreply.github.com>
2024-01-20 16:05:37 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        if(a!=b && b!=c && a!=c){
+            answer = a+b+c;
+        }else if(a==b && b!=c){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else if(a==c && c!=b){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else if(b==c && c!=a){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else if(a==b && b==c){
+            answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }
+        
+        return answer;
+    }
+}
